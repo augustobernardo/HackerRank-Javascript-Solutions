57976814b29f43634e80136179be9f6602f42168
Augusto Oliveira <gutooliveira027@gmail.com>
2023-02-14 16:24:12 -0300

    Refactoring the Sorting Solution

--- 30 Days of Code/Day 20 - Sorting/index.js
+++ 30 Days of Code/Day 20 - Sorting/index.js
@@ -33,20 +33,25 @@ function main() {
 
     // Write your code here
     var totalNum = 0
-
-    for (var i = 0; i < a.length; i++) {
-        var numberOfSwaps = 0
-        for (var j = 0; j < a.length -1; j++) {
+    
+    for (var i = 0; i < n; i++) {
+        // Track number of elements swapped during a single array traversal
+        var numberOfSwaps = 0;
+        
+        for (var j = 0; j < n - 1; j++) {
+            // Swap adjacent elements if they are in decreasing order
             if (a[j] > a[j + 1]) {
                 swap(a, j, j+1);
                 numberOfSwaps++;
             }
         }
         totalNum += numberOfSwaps;
+        // If no elements were swapped during a traversal, array is sorted
         if (numberOfSwaps == 0) {
             break;
         }
     }
+    
     console.log('Array is sorted in '+totalNum+ ' swaps.')
     console.log('First Element: '+a[0]);
     console.log('Last Element: '+a[a.length-1]);
